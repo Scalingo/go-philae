ba658da1baa4606356149d82a8a8ddce6d3ad1d9
Soulou <leo@unbekandt.eu>
2018-01-30 18:42:58 +0100

    Add changelog and use logger logging levels

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,21 @@
 # Changelog
 
+## v2.0.0
+
+* Add ability to configure logger and verbosity of philaehandler
+
+Breaking Change:
+
+```
+philaehandler.NewPhilaeRouter(http.Router, *prober.Prober)
+philaehandler.NewHandler(*prober.Prober)
+
+// become
+
+philaehandler.NewPhilaeRouter(http.Router, *prober.Prober, philaehandler.HandlerOpts)
+philaehandler.NewHandler(*prober.Prober, philaehandler.HandlerOpts)
+```
+
 ## v1.0.0
 
 * Initial release (semver version|to use with dep)
--- philaehandler/philaehandler.go
+++ philaehandler/philaehandler.go
@@ -11,9 +11,8 @@ import (
 )
 
 type PhilaeHandler struct {
-	prober  *prober.Prober
-	verbose bool
-	logger  logrus.FieldLogger
+	prober *prober.Prober
+	logger logrus.FieldLogger
 }
 
 func (handler PhilaeHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
@@ -22,24 +21,26 @@ func (handler PhilaeHandler) ServeHTTP(response http.ResponseWriter, request *ht
 	json.NewEncoder(response).Encode(result)
 	duration := time.Now().Sub(start)
 
-	if (handler.verbose && result.Healthy) || !result.Healthy {
-		handler.logger.WithFields(logrus.Fields{
-			"duration": duration.String(),
-			"healthy":  result.Healthy,
-		}).Info()
+	l := handler.logger.WithFields(logrus.Fields{
+		"source":   "philae",
+		"duration": duration.String(),
+		"healthy":  result.Healthy,
+	})
+	if result.Healthy {
+		l.Debug()
+	} else {
+		l.Info()
 	}
 }
 
 type HandlerOpts struct {
-	Verbose bool
-	Logger  logrus.FieldLogger
+	Logger logrus.FieldLogger
 }
 
 func NewHandler(prober *prober.Prober, opts HandlerOpts) http.Handler {
 	h := PhilaeHandler{
-		prober:  prober,
-		logger:  opts.Logger,
-		verbose: opts.Verbose,
+		prober: prober,
+		logger: opts.Logger,
 	}
 	if h.logger != nil {
 		h.logger = logrus.New()
