c87c15501e0155fb0834c372d36ebb6074031d18
Maxime CAISEZ <maxime@scalingo.com>
2023-03-01 09:50:17 +0100

    feat(elasticsearch-probe): use functionnal pattern for probe options

--- elasticsearchprobe/elasticsearchprobe.go
+++ elasticsearchprobe/elasticsearchprobe.go
@@ -17,26 +17,35 @@ type ElasticsearchProbe struct {
 	insecure bool
 }
 
-// NewElasticsearchProbe instantiate a new elasticsearch probe:
-// - name: probe name
-// - url : connection string with the form "http://username:password@example.com"
-// - caCert: CA certificate used for the TLS configuration
-func NewElasticsearchProbeWithTLS(name, url string, caCert []byte) ElasticsearchProbe {
-	return ElasticsearchProbe{
-		name:     name,
-		url:      url,
-		insecure: false,
-		caCert:   caCert,
+type ProbeOpts func(*ElasticsearchProbe)
+
+func WithInsecureSkipVerify() ProbeOpts {
+	return func(esProbe *ElasticsearchProbe) {
+		esProbe.insecure = true
 	}
 }
 
-func NewElasticsearchProbe(name, url string) ElasticsearchProbe {
-	return ElasticsearchProbe{
+func WithCA(caCert []byte) ProbeOpts {
+	return func(esProbe *ElasticsearchProbe) {
+		esProbe.caCert = caCert
+	}
+}
+
+// NewElasticsearchProbe instantiate a new elasticsearch probe:
+// - name: probe name
+// - url : connection string with the form "http://username:password@example.com"
+// - opts: optionnal parameters such as providing a TLS CA certificate or allowing insecure connections
+func NewElasticsearchProbe(name, url string, opts ...ProbeOpts) ElasticsearchProbe {
+	esProbe := ElasticsearchProbe{
 		name:     name,
 		url:      url,
-		insecure: true,
+		insecure: false,
 		caCert:   []byte(""),
 	}
+	for _, opt := range opts {
+		opt(&esProbe)
+	}
+	return esProbe
 }
 
 func (p ElasticsearchProbe) Name() string {
